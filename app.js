d010a46a0682624def882a76c6e7055028aa5766
ushiovii@hotmail.com <ushiovii@hotmail.com>
2022-06-23 23:44:31 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+/* Recursos Estaticos */
+app.use(express.static('public'));
+
+/* Rutas */
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}/`));
