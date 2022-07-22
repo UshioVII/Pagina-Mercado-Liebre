44002c9c5d9d607c2bfb8d85ed0306c82787d072
Diego Montes <ushiovii@hotmail.com>
2022-07-22 16:09:59 -0300

    Correción de codigo linea 38 y 39

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static('public'));
 
 /* Rutas */
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get("./register", (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}/`))
